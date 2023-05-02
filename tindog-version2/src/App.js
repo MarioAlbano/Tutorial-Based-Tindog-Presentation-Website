169046458a4f5295961c38945ab20a0614e3e4bd
Mario Albano <marioalbanon@gmail.com>
2023-05-02 19:39:39 -0300

    docs: cleared files, added bootstrap, sass

--- tindog-version2/src/App.js
+++ tindog-version2/src/App.js
@@ -1,10 +1,11 @@
+import React, { Component } from "react";
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div>
-      <header>
-        
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ tindog-version2/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header className="container text-center">
+        <img src="/logo.png" alt="Imagem Logo Empresa" />
+        <h1>Teste</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
